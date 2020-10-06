046a86edfb213ccc7296f1a82d799f9bce46079b
William Broussard <wbroussard@me.com>
2020-10-05 19:36:39 -0500

    fixed background image

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -3,7 +3,12 @@ import React from "react";
 function Jumbotron({ children }) {
   return (
     <div
-      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center", color:"white", backgroundImage:`url(https://freerangestock.com/sample/87961/man-crafting-wood.jpg)`}}
+      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center", color:"white", 
+      width: "100%", backgroundImage: "center",
+      backgroundImage:`url(https://i.postimg.cc/90Z4Fv8s/Wood-Work-Carpentry-scaled-e1583393107959.jpg)`,
+      backgroundSize: 'cover',
+      overflow: 'hidden',
+    }}
       className="jumbotron" 
     >
       {children}
