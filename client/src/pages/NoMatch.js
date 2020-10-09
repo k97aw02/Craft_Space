8ff75352134612df5c5e9e97071da479950ba38c
letsgitthis <jbentley1011@gmail.com>
2020-10-09 17:09:29 -0500

    adding authentication

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,31 +1,42 @@
 import React from "react";
-import { Col, Row, Container } from "../components/Grid";
 import { Link } from "react-router-dom";
-import Jumbotron from "../components/Jumbotron";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Footer from '../components/Footer/';
 
 function NoMatch() {
   return (
-    <Container fluid>
-      <Row>
-        <Col size="md-12">
-          <Jumbotron>
-            <h1>This page does not exist</h1>
+    <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '40vh' }}>
+      <Grid item md={12}>
+        <Box bgcolor="info.main" >
+          <React.Fragment>
+            <CssBaseline />
+
+            <div style={{
+              textAlign: "center",
+              height: "300px",
+              width: "flex",
+              color: "white",
+              marginTop: "0",
+              backgroundImage: `url(https://freerangestock.com/sample/87961/man-crafting-wood.jpg)`
+            }}>
+
+            </div>
+            <h1>You're leaving?</h1>
             <h1>
               <span role="img" aria-label="Dissapointed Face">
                 😢
               </span>
             </h1>
-            <Row>
-          <Col size="md-12">
+
             <Link to="/">Go to Main Page</Link>
-          </Col>
-        </Row>
-          </Jumbotron>
-        </Col>
-        
-      </Row>
-      
-    </Container>
+            <Footer />
+          </React.Fragment>
+        </Box>
+      </Grid>
+    </Typography>
   );
 }
 
