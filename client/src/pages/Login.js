424ea4fb5fd6e3027421945196bb048996d034c8
k97aw02 <k97aw02@gmail.com>
2020-10-09 20:43:18 -0500

    add styling to login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,6 +2,9 @@ import React, { useCallback, useContext } from "react";
 import { withRouter, Redirect } from "react-router-dom";
 import app from "../setFirebase.js";
 import { AuthContext } from "../components/context/Auth";
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 
 const Login = ({ history }) => {
   const handleLogin = useCallback(
@@ -27,20 +30,24 @@ const Login = ({ history }) => {
   }
 
   return (
-    <div>
-      <h1>Log in</h1>
-      <form onSubmit={handleLogin}>
-        <label>
-          Email
-          <input name="email" type="email" placeholder="Email" />
-        </label>
-        <label>
-          Password
-          <input name="password" type="password" placeholder="Password" />
-        </label>
-        <button type="submit">Log in</button>
-      </form>
-    </div>
+    <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '40vh' }}>
+      <Grid item md={12}>
+        <div>
+          <h1 style={{font: "Arial", marginTop: "10px" }}>Login</h1>
+            <form onSubmit={handleLogin}>
+              <label style={{ fontWeight: "bold" }}>
+                 Email: 
+                 <input name="email" type="email" placeholder="Email" />
+              </label>
+              <label style={{ fontWeight: "bold" }}>
+                Password: 
+              <input name="password" type="password" placeholder="Password" />
+              </label>
+              <Button style={{ backgroundColor: '#A9A9A9' }} type="submit">Login</Button>
+            </form>
+        </div>
+      </Grid>
+    </Typography>
   );
 };
 
